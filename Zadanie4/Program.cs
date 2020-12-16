d6d08af1a40b09aa3f63fba848ab1611c211cb92
Jacek Kuźmicz <snwcore@gmail.com>
2020-12-16 07:48:55 +0100

    Dodaj pliki projektów.

---
+++ Zadanie4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zadanie4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
